4b6cb01ee615a184faf57bd5b73ae6309b6da30d
Qingbolan <yxjh2020.12.8@gmail.com>
2022-04-17 22:21:54 +0800

    first commit

---
+++ add.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+    <meta name="keywords" content="青博蓝,问心计划,匿名提问箱">
+    <meta name="description" content="天青色等烟雨,我在这里等你">
+    <meta name="author" content="Qingbolan">
+    <title>独上高楼，望尽天涯路</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="//interface.qingbolan.net/blog-source-2022-3-24/Qbl.ico/Qbl05.ico">
+    <link rel="stylesheet" href="//interface.qingbolan.net/blog-source-2022-3-24/css/font-awesome.min.css">
+</head>
+<script id="hexo-configurations">
+    let KEEP = window.KEEP || {};
+    KEEP.hexo_config = {
+        hostname: "little-box.qingbolan.net",
+        root: "/",
+        language: "zh-CN",
+    }, KEEP.theme_config = {
+        toc: {
+            enable: !0,
+            number: !0,
+            expand_all: !0,
+            init_open: !0
+        },
+        style: {
+            primary_color: "#0066CC",
+            favicon: "//interface.qingbolan.net/blog-source-2022-3-24/Qbl.ico/Qbl05.ico",
+            article_img_align: "left",
+            left_side_width: "260px",
+            content_max_width: "920px",
+            hover: {
+                shadow: !0,
+                scale: !0
+            },
+            first_screen: {
+                enable: !0,
+                background_img: "/images/bg.svg",
+                description: "衣带渐宽终不悔，为伊消得人憔悴."
+            },
+            scroll: {
+                progress_bar: {
+                    enable: 0
+                },
+                percent: {
+                    enable: 0
+                }
+            }
+        },
+        local_search: {
+            enable: 0,
+            preload: !0
+        },
+        code_copy: {
+            enable: 0,
+            style: "default"
+        },
+        pjax: {
+            enable: 0
+        },
+        lazyload: {
+            enable: !0
+        }
+    }, KEEP.language_ago = {
+        second: "%s 秒前",
+        minute: "%s 分钟前",
+        hour: "%s 小时前",
+        day: "%s 天前",
+        week: "%s 周前",
+        month: "%s 个月前",
+        year: "%s 年前"
+    }
+</script>
+<meta name="generator" content="Hexo 6.0.0">
+
+<body>
+    <div class="progress-bar-container"><span class="scroll-progress-bar"></span> <span class="pjax-progress-bar"></span> <span class="pjax-progress-icon"><i class="fas fa-circle-notch fa-spin"></i></span></div>
+    <main class="page-container">
+        <div class="first-screen-container flex-center fade-in-down-animation">
+            <div class="content flex-center">
+                <div class="description">
+                    <div class="block"  style="display: block;">
+                    
+                    </div>
+                </div>
+                <div class="s-icon-list">
+                    <p>#2022-4-16</p>
+                </div>
+            </div>
+        </div>
+        <div class="page-main-content">
+        <form role="form" action="#" method="post">
+                        <textarea name="content" rows="6" cols="25" autocomplete="off" required style="border: 1px solid #ccc;
+					  box-shadow: 1px 1px 1px #999;"></textarea>
+                        <div>
+                            <a href="index.php" style="font-size:10;display:block;">放弃编辑查看已有问题或留言</a>
+                            <hr>
+                            <button type="submit" name="pubMsg" class="btn btn-default">编辑完成</button>
+                    </form>
+            <div class="page-main-content-bottom">
+                <footer class="footer">
+                    <div class="info-container">
+                        <div class="copyright-info info-item">&copy; <span>2022</span> - 2022&nbsp;<i class="fas fa-heart icon-animate"></i>&nbsp;<a href="/">Qingbolan</a></div>
+                        <div class="website-count info-item"><span id="lf_days"></span>天<span id="lf_hours"></span>时<span id="lf_minutes"></span>分<span id="lf_seconds"></span>秒</div>
+                        <script>
+                            const second = 1e3,
+                                minute = 6e4,
+                                hour = 36e5,
+                                day = 24 * hour;
+                            let countDown = new Date("Dec 21, 2020 11:21:00").getTime(),
+                                x = setInterval((function() {
+                                    let e = (new Date).getTime() - countDown;
+                                    document.getElementById("lf_days").innerText = Math.floor(e / day), document.getElementById("lf_hours").innerText = Math.floor(e % day / hour), document.getElementById("lf_minutes").innerText = Math.floor(e % hour / 6e4), document.getElementById("lf_seconds").innerText = Math.floor(e % 6e4 / 1e3)
+                                }), 1e3)
+                        </script>
+                        <a target="_blank" rel="noopener" href="https://clustrmaps.com/site/1bn20" title="Visit tracker"><img src="//www.clustrmaps.com/map_v2.png?d=ZwiPWQhyTJTyc67FIlwmQz2-9uMHLpDz7P15Q94k1Yk&cl=ffffff"></a>
+                        <div class="icp-info info-item"><a target="_blank" rel="nofollow" href="https://beian.miit.gov.cn">京ICP备2020047636号-1</a></div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <div class="right-bottom-side-tools">
+            <div class="side-tools-container">
+                <ul class="side-tools-list">
+                    <li class="tools-item tool-font-adjust-plus flex-center"><i class="fas fa-search-plus"></i></li>
+                    <li class="tools-item tool-font-adjust-minus flex-center"><i class="fas fa-search-minus"></i></li>
+                    <li class="tools-item tool-expand-width flex-center"><i class="fas fa-arrows-alt-h"></i></li>
+                    <li class="tools-item tool-dark-light-toggle flex-center"><i class="fas fa-moon"></i></li>
+                    <li class="tools-item tool-scroll-to-bottom flex-center"><i class="fas fa-arrow-down"></i></li>
+                </ul>
+                <ul class="exposed-tools-list">
+                    <li class="tools-item tool-toggle-show flex-center"><i class="fas fa-cog fa-spin"></i></li>
+                    <li class="tools-item tool-scroll-to-top flex-center"><i class="arrow-up fas fa-arrow-up"></i> <span class="percent"></span></li>
+                </ul>
+            </div>
+        </div>
+        <div class="image-viewer-container"><img src=""></div>
+        <div class="search-pop-overlay">
+            <div class="popup search-popup">
+                <div class="search-header"><span class="search-input-field-pre"><i class="fas fa-keyboard"></i></span>
+                    <div class="search-input-container"><input autocomplete="off" autocorrect="off" autocapitalize="off" placeholder="搜索..." spellcheck="false" type="search" class="search-input"></div><span class="popup-btn-close"><i class="fas fa-times"></i></span>
+                </div>
+                <div id="search-result">
+                    <div id="no-result"><i class="fas fa-spinner fa-pulse fa-5x fa-fw"></i></div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <div class="post-scripts pjax"></div>
+    <script src="//interface.qingbolan.net/blog-source-2022-3-24/js/utils.js"></script>
+    <script src="//interface.qingbolan.net/blog-source-2022-3-24/js/main.js"></script>
+    <script src="//interface.qingbolan.net/blog-source-2022-3-24/js/header-shrink.js"></script>
+    <script src="//interface.qingbolan.net/blog-source-2022-3-24/js/back2top.js"></script>
+    <script src="//interface.qingbolan.net/blog-source-2022-3-24/js/dark-light-toggle.js"></script>
+    <script src="//interface.qingbolan.net/blog-source-2022-3-24/js/lazyload.js"></script>
+    <div class="post-scripts pjax"></div>
+    <script src="//interface.qingbolan.net/blog-source-2022-3-24/js/libs/pjax.min.js"></script>
+    <script>
+        window.addEventListener("DOMContentLoaded", () => {
+            window.pjax = new Pjax({
+                selectors: ["head title", ".page-container", ".pjax"],
+                history: !0,
+                debug: !1,
+                cacheBust: !1,
+                timeout: 0,
+                analytics: !1,
+                currentUrlFullReload: !1,
+                scrollRestoration: !1
+            }), document.addEventListener("pjax:send", () => {
+                KEEP.utils.pjaxProgressBarStart()
+            }), document.addEventListener("pjax:complete", () => {
+                KEEP.utils.pjaxProgressBarEnd(), window.pjax.executeScripts(document.querySelectorAll("script[data-pjax], .pjax script")), KEEP.refresh()
+            })
+        })
+    </script>
+</body>
